8f1b8bf733f4f2e14d4b9a64f3c3d5f2b28ac92e
thecasualdev <git@thecasual.dev>
2025-08-27 17:56:57 +1000

    A

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,8 +1,19 @@
 
 export const actions = {
     default: async ({ request }) => {
+
         const data = await request.formData();
         const mail = data.get("mail");
         console.log(mail)
+
+        await fetch (
+            "https://discord.com/api/webhooks/1410131814957453392/DZWwTncoAHwMc5tk1iiWEkaaxg6u11lVxv4wiDqi4goxRY24pCSJ4gQi2OxzVAQQ6WlV",
+            {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ content: mail })
+            }
+        );
+
     }
 };

e44ef577e7ef0d647ed51d5c77e781a30ec6f991
thecasualdev <git@thecasual.dev>
2025-08-27 19:01:44 +1000

    Added contact form

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -5,15 +5,42 @@ export const actions = {
     default: async ({ request }) => {
 
         const data = await request.formData();
-        const mail = data.get("mail");
-        console.log(mail)
+        const name = data.get("name")
+        const mail = data.get("mail")
+
+
+        const message = data.get("message")
+
+        const payload = {
+            embeds: [
+                {
+                    color: 10134510,
+                    fields: [
+                        {
+                           name: "Name:",
+                           value: `\`\`\`${name}\`\`\`` ,
+                           inline: true
+                        },
+                        {
+                           name: "Email:",
+                           value: `||\`\`\`${mail}\`\`\`||`,
+                           inline: true
+                        },
+                        {
+                            name: "Message:",
+                            value: `\`\`\`${message}\`\`\``
+                        }
+                    ]
+                }
+            ]
+        }
 
         await fetch (
             DISCORD_WEBHOOK_URL,
             {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ content: mail })
+                body: JSON.stringify(payload)
             }
         );
 
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -6,7 +6,18 @@
 <h1>contact me here!</h1>
 
 <form method="POST">
+
     <label>
-        mail <input type="email" name="mail">
+        Name: <input type="text" name="name" required>
     </label>
+    <label>
+        Email: <input type="email" name="mail" required>
+    </label>
+
+    <label>
+        Message: <textarea name="message" required></textarea>
+    </label>
+
+    <button type="submit">Submit</button>
+
 </form>
